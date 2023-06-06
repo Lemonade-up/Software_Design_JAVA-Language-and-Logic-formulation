7aaba054599c998b646aa9d011ed097aaf8e91ea
Lemonade7 <wmaglangit1118@gmail.com>
2023-06-06 10:19:43 +0800

    added implementation of Array List

--- Number3.java
+++ Number3.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
 public class Number3 {
     public static void main(String[] args) {
         int n = 10;
         int t1 = 0, t2 = 1;
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        arr.add(t1);
+        arr.add(t2);
+
         System.out.print("First " + n + " terms: ");
         
-        for(int i = 3, )
+        System.out.print(arr);
     }
 }
