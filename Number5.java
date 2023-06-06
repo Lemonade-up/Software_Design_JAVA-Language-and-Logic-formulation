57e4a7578a8cbe9c2e9b3c9fe0cf40c5da5e3606
Lemonade7 <wmaglangit1118@gmail.com>
2023-06-06 11:22:11 +0800

    added loop to check each letter

--- Number5.java
+++ Number5.java
@@ -2,10 +2,15 @@ public class Number5 {
     public static void main(String[] args) {
         String original = "racecar";
         String reversed = "";
+        int len = original.length();
+        int lenIter = len/2;
+        boolean isPalindrome = true;
 
+        for(int i = 0; i <= lenIter-1; i++){
 
+        }
 
-        
+        System.out.print(isPalindrome);
 
 
 
