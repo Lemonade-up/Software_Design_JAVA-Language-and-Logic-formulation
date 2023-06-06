1c311402b9c9d643d56f29bf98edfc451b720ef4
Lemonade7 <wmaglangit1118@gmail.com>
2023-06-06 10:31:16 +0800

    added elements of array to get next number, done

--- Number3.java
+++ Number3.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 public class Number3 {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 11;
+        /*Nakalagay sa output hanggang 55, so tinaasan ko ng 1 yung n since pang 11th na yung 55*/
         int t1 = 0, t2 = 1;
+        int tAdd;
         
         ArrayList<Integer> arr = new ArrayList<>();
         
@@ -10,7 +12,13 @@ public class Number3 {
         arr.add(t2);
 
         System.out.print("First " + n + " terms: ");
-        
+        for(int i = 3; i <= n; i++){
+            int firstAdd = i-3;
+            int secondAdd = i-2;
+            tAdd = arr.get(firstAdd) + arr.get(secondAdd);
+            arr.add(tAdd);
+
+        }
         System.out.print(arr);
     }
 }
