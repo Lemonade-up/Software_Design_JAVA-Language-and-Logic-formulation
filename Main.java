f504ace50db059798650d8134b5adb5f2f2b3731
Lemonade7 <wmaglangit1118@gmail.com>
2023-06-06 09:45:10 +0800

    first commit ,created loop for number of elements in array

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+        int max = numbers[0];
+        int lenNum = numbers.length-1;
+        
+        for(int i = 0; i < lenNum; i++){
+            max = numbers[i];
+            System.out.println("The maximum value in the array is: " + max);
+        }
+            
+}
+        /*
+        Process here the max value will be save to max variable
+        use loop
+        */
+
+       
+}
