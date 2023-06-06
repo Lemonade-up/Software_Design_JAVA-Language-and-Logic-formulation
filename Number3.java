6c5667595d4460b197318aed467d439438537c68
Lemonade7 <wmaglangit1118@gmail.com>
2023-06-06 10:03:32 +0800

    added number3

---
+++ Number3.java
@@ -0,0 +1,9 @@
+public class Number3 {
+    public static void main(String[] args) {
+        int n = 10;
+        int t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
+        
+        for(int i = 3, )
+    }
+}
