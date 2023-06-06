f2b16a757ae4b01350dd301edd87ee645b374ef8
Lemonade7 <wmaglangit1118@gmail.com>
2023-06-06 11:35:39 +0800

    trying if else statements, cant get it to work

--- Number5.java
+++ Number5.java
@@ -4,13 +4,18 @@ public class Number5 {
         String reversed = "";
         int len = original.length();
         int lenIter = len/2;
-        boolean isPalindrome = true;
+        boolean isPalindrome = false;
 
-
+        for(int i = 0; i <= lenIter-1; i++){
+            if(original.substring(i,i+1) == original.substring(len-i-1,len-i)){
+                isPalindrome = true;
+                System.out.print(isPalindrome);
+            }
+            System.out.print(original.substring(i,i+1) +" and "+ original.substring(len-i-1,len-i));
+        }
 
         System.out.print(isPalindrome);
-        System.out.print(original.substring(0,1));
-        System.out.print(original.substring(len-1,len));
+
 
 
 
