ab24dc70d037f5bd354787abff7cffcb5af22656
Lemonade7 <wmaglangit1118@gmail.com>
2023-06-08 22:19:10 +0800

    added break if ispalindrome is false to fix error

--- Number5.java
+++ Number5.java
@@ -6,15 +6,21 @@ public class Number5 {
         int lenIter = len/2;
         String fLetter;
         String bLetter;
-        boolean isPalindrome = false;
+        boolean isPalindrome = true;
 
         for(int i = 0; i <= lenIter-1; i++){
+            
             fLetter = original.substring(i,i+1);
             bLetter = original.substring(len-i-1,len-i);
             isPalindrome = fLetter.equals(bLetter);
+            System.out.print(original + " is a Palindrome? "+isPalindrome);
+            if(isPalindrome == false){
+                break;
+            }             
+
         }
 
-        System.out.print(original + " is a Palindrome? "+isPalindrome);
+        System.out.print(original + " is a Palindrome? "+ isPalindrome);
 
 
 
